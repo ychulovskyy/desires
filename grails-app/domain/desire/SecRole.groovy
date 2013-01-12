a3f389fe22f0c972db00c81fc4604b0159e5ac4a
Yuriy Chulovskyy <yuriy.chulovskyy@gmail.com>
2013-01-12 03:23:54 +0200

    Added security

---
+++ grails-app/domain/desire/SecRole.groovy
@@ -0,0 +1,14 @@
+package desire
+
+class SecRole {
+
+    String authority
+
+    static mapping = {
+        cache true
+    }
+
+    static constraints = {
+        authority blank: false, unique: true
+    }
+}
